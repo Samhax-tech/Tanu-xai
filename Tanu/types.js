2fbd182f3245226fb761853870bf302b2759fcc1
Samhax official <191114773+Samhax-tech@users.noreply.github.com>
2026-09-07 17:36:25 +0500

    Delete Tanu/types.js

--- Tanu/types.js
+++
@@ -1,6 +0,0 @@
-const PERMANENT_OWNERS = Object.freeze([
-  { name: 'Arman HTX', number: '256788028745', role: 'Professional Dev' },
-  { name: 'Tanu Darling', number: '919864179454', role: "Owner's Owner and Wife" }
-]);
-
-module.exports = { PERMANENT_OWNERS };
